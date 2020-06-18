66b02d48161cc2c18335d4cb0d92dcd32e2bde59
nitinnsharma234 <62827780+nitinnsharma234@users.noreply.github.com>
2020-06-18 20:26:29 +0530

    Create Max_Sum_of_Subtree_in_Binary_Tree.cpp

---
+++ Binary Tree -api/Max_Sum_of_Subtree_in_Binary_Tree.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+     int data, sum;
+     Node*left;
+     Node*right;
+     Node(int x)
+     {
+          data=x;
+          left=NULL;
+          right=NULL;
+     }
+};
+
+int  assign_sum(Node*temp)
+{static int  maxsum=INT_MIN,sum;
+    if (!temp)
+    {
+        return 0 ;
+    }
+    if (!temp->left && !temp->right )
+    {
+        temp->sum=0;
+        return temp->data;
+    }
+    int l=assign_sum(temp->left);
+    int k=assign_sum(temp->right);
+    temp->sum=(l+k+temp->left->sum+temp->right->sum);
+    if (temp->sum+temp->data >maxsum)
+    {
+         maxsum=temp->sum+temp->data;
+         cout<<maxsum<<endl;
+    }
+    return temp->data;
+}
+void inorder_printing(Node*temp)
+{
+     if(!temp)
+     {return ;}
+     inorder_printing(temp->left);
+     cout<<temp->data<<"-->"<<temp->sum<<endl;
+     inorder_printing(temp->right);
+}
+int main()
+{
+ Node*root=new Node(1);
+ root->left=new Node(-2);
+ root->left->left=new Node(4);
+ root->left->right=new Node(-58);
+ 
+ root->right= new Node(3);
+ root->right->left=new Node(-6);
+ root->right->right=new Node(2);
+ assign_sum(root);
+ inorder_printing(root);
+ //cout<<root->left->sum;
+}
